fdb47a9cc720158626d64fd21041568c98849019
Mouad <mouadse@gmail.com>
2025-03-14 02:57:47 +0000

    Fixing our parser step by step and trying to do a whole parsing in one chain of logic

--- src/parse_and_validate/parser.c
+++ src/parse_and_validate/parser.c
@@ -64,3 +64,46 @@ void	trim_trailing_whitespace(char *str, char *line)
 	}
 	str[i - whitespace_count] = '\0';
 }
+
+void	store_texture_path(char **texture_ptr, char *raw_text, char *identifier,
+		char *line_buffer)
+{
+	int		count;
+	char	*path_start;
+
+	count = 0;
+	if (*texture_ptr != NULL)
+	{
+		ft_putstr_fd("Error\n", 2);
+		ft_putstr_fd("Duplicate identifier: ", 2);
+		ft_putstr_fd(identifier, 2);
+		ft_putstr_fd(" in line: ", 2);
+		ft_putstr_fd(line_buffer, 2);
+		ft_putstr_fd("\n", 2);
+		exit(ONE);
+	}
+	validate_spacing(raw_text, identifier, line_buffer);
+	path_start = raw_text;
+	while (ft_isspace(*path_start) || ft_strncmp(identifier, path_start,
+			2) == 0)
+	{
+		if (ft_strncmp(identifier, path_start, 2) == 0)
+		{
+			count++;
+			path_start++;
+		}
+		path_start++;
+	}
+	trim_trailing_whitespace(path_start, line_buffer);
+	if (!file_exists(path_start) || count != 1)
+	{
+		ft_putstr_fd("Error\n", 2);
+		ft_putstr_fd("Invalid path for identifier: ", 2);
+		ft_putstr_fd(identifier, 2);
+		ft_putstr_fd(" in line: ", 2);
+		ft_putstr_fd(line_buffer, 2);
+		ft_putstr_fd("\n", 2);
+		exit(ONE);
+	}
+	*texture_ptr = ft_strdup(path_start);
+}
--- src/parse_and_validate/parser_utils.c
+++ src/parse_and_validate/parser_utils.c
@@ -1,57 +1,61 @@
 #include "../../include/cub3d.h"
 
-int ft_isspace(char c) {
-  return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' ||
-          c == '\r');
+int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
+		|| c == '\r');
 }
 
-int starts_with(char *str, char *prefix) {
-
-  if (!str || !prefix)
-    return (0);
-  while (*prefix) {
-    if (*str != *prefix)
-      return (0);
-    str++;
-    prefix++;
-  }
-  return (1);
+int	starts_with(char *str, char *prefix)
+{
+	if (!str || !prefix)
+		return (0);
+	while (*prefix)
+	{
+		if (*str != *prefix)
+			return (0);
+		str++;
+		prefix++;
+	}
+	return (1);
 }
 
-int is_empty_line(char *line) {
-  if (!line) // This is a guard clause but it can be removed since we check for
-    return (1); // NULL in another function
-
-  while (*line) {
-    if (!ft_isspace(*line))
-      return (0);
-    line++;
-  }
-  return (1);
+int	is_empty_line(char *line)
+{
+	if (!line)
+		// This is a guard clause but it can be removed since we check for
+		return (1); // NULL in another function
+	while (*line)
+	{
+		if (!ft_isspace(*line))
+			return (0);
+		line++;
+	}
+	return (1);
 }
 
-int file_exists(char *file_path) {
-  int fd;
+int	file_exists(char *file_path)
+{
+	int	fd;
 
-  if (!file_path)
-    return (0);
-
-  fd = open(file_path, O_RDONLY);
-  if (fd < 0)
-    return (0);
-
-  close(fd);
-  return (1);
+	if (!file_path)
+		return (0);
+	fd = open(file_path, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	close(fd);
+	return (1);
 }
 
-int is_map_line(char *line) {
-  if (!line || !*line)
-    return (0);
-
-  while (*line) {
-    if (!is_valid_map_char(*line))
-      return (0);
-    line++;
-  }
-  return (1);
+int	is_map_line(char *line)
+{
+	if (!line || !*line)
+		return (0);
+	while (*line)
+	{
+		if (!is_valid_map_char(*line))
+			return (0);
+		line++;
+	}
+	return (1);
 }
